0f7b32c3e2ab519123fd06889143494d56afc315
zohar azaria <azaria.a.z218@gmail.com>
2025-06-22 16:12:18 +0300

    הזמנים התקצרו

--- Task1SectionB/ReadCSV_Process.py
+++ Task1SectionB/ReadCSV_Process.py
@@ -4,26 +4,71 @@ import pandas as pd
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from validation_and_meanHour import *
 from OpenFile import *
+import glob
+
+
 
 def SpliteCSVFileByChunks(file_path, chunksize=10000):
     os.makedirs("daily_chunks", exist_ok=True)
     file_paths = set()  # שימוש בסט כדי למנוע כפילויות
+    file_ext = os.path.splitext(file_path)[1].lower()
+# מחיקת קבצים ישנים מהפיצול הקודם
+    for f in glob.glob("daily_chunks/data_*.csv"):
+        os.remove(f)
 
 
-    # file_paths = []  # שלב 1: רשימה ריקה לשמירת הנתיבים
-    reader=Open_With_Chunk(file_path,chunksize)
-    for chunk in reader:
-        chunk=validation(chunk)
-        
-# חלוקה לפי יום ושמירה לכל קובץ
-        for day, group in chunk.groupby(chunk['timestamp'].dt.date):
+    def save_grouped_by_day(df):
+        df = validation(df)
+        for day, group in df.groupby(df['timestamp'].dt.date):
             file_name = f"daily_chunks/data_{day}.csv"
             write_mode = 'a' if os.path.exists(file_name) else 'w'
             header = not os.path.exists(file_name)
             group.to_csv(file_name, index=False, mode=write_mode, header=header)
-
             file_paths.add(file_name)
+
+    if file_ext == '.csv':
+        # file_paths = []  # שלב 1: רשימה ריקה לשמירת הנתיבים
+           
+            for chunk in  pd.read_csv(
+                    file_path,
+                    chunksize=chunksize,
+                    names=["timestamp", "value"],
+                    # header=None,
+                    # skiprows=1,
+                    low_memory=False,
+                    parse_dates=['timestamp'],
+
+                ):
+                save_grouped_by_day(chunk)
+
+    elif file_ext in ('.parquet', '.pq'):
+         df = pd.read_parquet(file_path, columns=["timestamp", "value"], engine="fastparquet")
+
+         df.columns = ["timestamp", "value"]  # אם אין כותרות
+         save_grouped_by_day(df)
+    else:
+        raise ValueError(f" format is not supported: {file_ext}")
+
     return file_paths
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
